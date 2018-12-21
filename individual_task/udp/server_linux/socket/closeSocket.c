a2459662131b076ce4c0a9594943e7ee0ff8c44d
mrololo <mr.kostman@gmail.com>
2018-12-22 00:32:50 +0300

    delete trash from repo

--- individual_task/udp/server_linux/socket/closeSocket.c
+++
@@ -1,7 +0,0 @@
-#include "sock.h"
-
-void closeSocket(int sockfd) {
-    printf(CLOSE_SOCK, sockfd);
-    shutdown(sockfd, 2);
-    close(sockfd);
-}
--- individual_task/udp/server_linux/socket/getClearMsg.c
+++
@@ -1,5 +0,0 @@
-#include "sock.h"
-
-void getClearMsg(char* buf) {
-	strcpy(buf, buf + 2);
-}
